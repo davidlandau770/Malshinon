d93211d7d1aac146358023818d9e7622df5ad088
davidlandau770 <dal778770@gmail.com>
2025-06-11 20:33:58 +0300

    finish

--- Program.cs
+++ Program.cs
@@ -11,21 +11,6 @@ namespace Malshinon
         static async Task Main(string[] args)
         {
             DAL dAL = new DAL();
-            //dAL.PrintPeople();
-
-            //People people = new People(101, "david", "landau");
-            //People people2 = new People(100, "david114", "landau221");
-            //dAL.InsertPeople(people2);
-
-            //dAL.PersonIdentificationFlow("david landau");
-            //dAL.PersonIdentificationFlow("david landau3");
-            //dAL.PersonIdentificationFlow("david landau4");
-            //dAL.PersonIdentificationFlow("david avishay landau");
-            //dAL.PersonIdentificationFlow("david landau");
-            //dAL.PersonIdentificationFlow("david a landau");
-            //dAL.PersonIdentificationFlow("david av landau");
-            //dAL.PersonIdentificationFlow("david avi landau");
-            //dAL.PersonIdentificationFlow("david avis landau");
             MenuToUser menuToUser = new MenuToUser();
             await menuToUser.StartProgram();
         }
